54b6af07e3439756dc7dff32bd872d2c062dd11b
Pablo Zallio <pablozalliodev@gmail.com>
2026-04-08 09:52:19 +0200

    modified file app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
-import { useEffect } from "react"
-import { apiProductRepository } from "./infrastructure/repositories/apiProductRepository"
-
-
-
 
 
 const App = () => {
 
 
 
-      useEffect(() => {
-
-            const prueba = async () => {
-                  apiProductRepository()
-                  const response = await apiProductRepository().getAllProducts()
-                  console.log(response)
-            }
-            prueba()
-      }, [])
-
+   
 
       return (
-            <div>App</div>
+            <div>
+                  
+            </div>
       )
 }
 
