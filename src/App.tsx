2949622714f69d6b570a9ff02d7e6008e607fa58
Pablo Zallio <pablozalliodev@gmail.com>
2026-04-12 14:42:32 +0200

    [FEAT] - Add a footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import Footer from "./presentation/Components/layouts/Footer"
 import Header from "./presentation/Components/layouts/Header"
 import AppRouter from "./presentation/Router/AppRouter"
 
 
 const App = () => {
-  return (
-    <>
-    <Header />
-    <AppRouter/>
-      
-    </>
-  )
+      return (
+            <>
+                  <Header />
+                  <AppRouter />
+                  <Footer />
+            </>
+      )
 }
 
 export default App
